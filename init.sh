f40d96d738b910c5a371620480f532ee6a94fb34
Ubuntu <ubuntu@ip-172-26-0-223.eu-west-2.compute.internal>
2017-07-16 16:49:15 +0000

    adjust file endings

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get -y upgrade
+#sudo apt-get update
+#sudo apt-get -y upgrade
+
 # To review how to avoid GRUB / homelst issue
 # sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
 
