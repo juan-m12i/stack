bbe5c9b6869b107f5a3b33109650b761942ce250
juan-m12i <juan-m12i@users.noreply.github.com>
2017-07-16 17:30:40 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get update
 sudo apt-get -y upgrade
-
 # To review how to avoid GRUB / homelst issue
 # sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
 
-sudo apt-get install git -y
-git clone https://github.com/juan-m12i/stack.git
+#sudo apt-get install git -y
+#git clone https://github.com/juan-m12i/stack.git
+
 sudo apt-get install -y python3-pip -y
 pip3 install virtualenvwrapper
